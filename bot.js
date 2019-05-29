65c941bab509a1eae2d9edafdfbf1b8ae88032b3
iedenis <denis.ievlev@gmail.com>
2019-05-29 05:20:11 +0300

    receiving and uploading image

--- bot.js
+++ bot.js
@@ -1,11 +1,15 @@
 const token = process.env.TOKEN;
 const dbp = process.env.DB_PASSWORD;
 const dbun = process.env.DB_USERNAME;
+const algorithmia_key = process.env.ALGORITHMIA_KEY;
+
 const Bot = require('node-telegram-bot-api');
+const algorithmia = require("algorithmia");
+const algorithmia_client = algorithmia(algorithmia_key);
 
 let bot;
 let myDB;
- 
+
 
 if (process.env.NODE_ENV === 'production') {
   bot = new Bot(token);
@@ -18,7 +22,6 @@ else {
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
 const MongoClient = require('mongodb').MongoClient;
-console.log("USERNAME: "+dbun)
 const uri = `mongodb+srv://${dbun}:${dbp}@cluster0-8tttz.mongodb.net/test?retryWrites=true`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true });
@@ -27,7 +30,6 @@ client.connect((err, database) => {
   else {
     myDB = database.db("cars");
   }
-
 })
 
 const isNumber = (number, callback) => {
@@ -36,21 +38,55 @@ const isNumber = (number, callback) => {
   if (platePattern.test(number)) { callback(null, number) }
   else callback("זה לא מספר של רכב", null);
 }
+const fileUpload = function (local_file_path) {
+  console.log('trying to upload the file ',local_file_path)
+  const fileName = local_file_path.split('/').pop();
+  const uploadedFilePath = `data://.session/${fileName}`
+  const directory = 'data://.session'
 
-bot.on('message', (msg) => {
-  const number = msg.text;
-  let reply;
-  isNumber(number, function (err, number) {
-    if (err) bot.sendMessage(msg.chat.id, err).catch(err => console.log(err))
-    else {
-      const collection = myDB.collection('tavim')
-      const retval = collection.findOne({ "MISPAR RECHEV": number }).then(function (result) {
-        if (result) reply = `לרכב ${number} *יש* תו חניה נכה`
-        else reply = `לרכב ${number} *אין* תו חניה נכה`
-        bot.sendMessage(msg.chat.id, reply, { parse_mode: 'Markdown' })
-      }).catch(err => console.log(err))
+  algorithmia_client.file(uploadedFilePath).exists(function (exists) {
+    if (!exists) {
+      directory.putFile(local_file_path, function(response){
+        if(response.error){
+          console.log('fail to upload file '+local_file_path)
+        }
+        else{
+          console.log('file successfully uploaded');
+        }
+      })
     }
   })
+
+}
+// data://.session/:filename// temoporary files here
+
+bot.on('message', (msg) => {
+  if (msg.photo) {
+    const photo = msg.photo;
+    const fileId = photo[2].file_id;
+
+    bot.downloadFile(fileId, __dirname + '/images/')
+      .then(path => fileUpload(path))
+      .catch(err => console.log(err))
+  }
+  else if (msg.text) {
+    const number = msg.text;
+    let reply;
+    isNumber(number, function (err, number) {
+      if (err) bot.sendMessage(msg.chat.id, err).catch(err => console.log(err))
+      else {
+        const collection = myDB.collection('tavim')
+        const retval = collection.findOne({ "MISPAR RECHEV": number }).then(function (result) {
+          if (result) reply = `לרכב ${number} *יש* תו חניה נכה`
+          else reply = `לרכב ${number} *אין* תו חניה נכה`
+          bot.sendMessage(msg.chat.id, reply, { parse_mode: 'Markdown' })
+        }).catch(err => console.log(err))
+      }
+    })
+  }
+  else {
+    console.log('כדי להשתמש בבוט תזין בבקשה מספר רכב או תעלה תמונה')
+  }
 });
 
 module.exports = bot;
